94eab73dc09326b3cad02225674c0aab676b5ae8
PabluskiNC <PabluskiNC@users.noreply.github.com>
2016-05-22 21:12:05 -0400

    Update pins.h

--- pins.h
+++ pins.h
@@ -8,4 +8,8 @@ const int rtcSDA        = D3; // Real Time Clock SDA (i2c ESP8266 NodeMCU 0.9)
 const int rtcSCL        = D4; // Real Time Clock SCL (i2c ESP8266 NodeMCU 0.9)
 const int doorClose     = D5; // MotorA CW DOWN
 const int doorOpen      = D6; // MotorA CCW UP
+const int fanControl    = D7;
+const int heatControl   = D8;
+const int onewirebus    = D9;
+
 #define TRIGGER_PIN D0
