e62b0147be333b7eb52aad299b4b192288922d3a
PabluskiNC <PabluskiNC@users.noreply.github.com>
2016-05-22 21:03:44 -0400

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,10 @@
+// WIFI Settings
+#define wHost "ssid"
+#define wPass "password"
+
+// MQTT Host Settings
+#define mHost "example.com"
+#define mPort 1883
+#define mClientID "coop-duino" 
+#define mUsername "chicken_coop"
+#define mPassword "secret"
