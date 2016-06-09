1a46357cf917b72372b326cc0f69e93b68373e09
PabluskiNC <PabluskiNC@users.noreply.github.com>
2016-06-09 10:21:59 -0400

    Update options.h

--- options.h
+++ options.h
@@ -1,5 +1,10 @@
 // This option will enable the LCD code
 #define LCD_DISPLAY
+// LCD parameters
+#ifdef LCD_DISPLAY
+#define LCD_ROWS 4
+#define LCD_COLS 20
+#endif
 
 // print debug messages or not to serial
 int Debugging = 1;
