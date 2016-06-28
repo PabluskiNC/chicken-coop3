d9e3fc0571947664d1b4df9125bbf416d5c95f86
PabluskiNC <PabluskiNC@users.noreply.github.com>
2016-06-28 12:01:59 -0400

    Update options.h

--- options.h
+++ options.h
@@ -1,6 +1,10 @@
+// uncomment to turn on debug
+//#define DEBUG_ESP_CORE
+
 // This option will enable the LCD code
 #define LCD_DISPLAY
-// LCD parameters
+
+// LCD parameters 
 #ifdef LCD_DISPLAY
 #define LCD_ROWS 4
 #define LCD_COLS 20
